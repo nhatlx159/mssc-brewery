e7d2eb5042b48978f56464ed81f0b25f760df82f
Vermillion <nhatlx159@gmail.com>
2023-12-29 19:39:54 +0700

    build CustomerDto

---
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -0,0 +1,27 @@
+package guru.springframework.msscbrewery.web.controller;
+
+import guru.springframework.msscbrewery.services.CustomerService;
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/customer")
+@Controller
+public class CustomerController {
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping({"/{id}"})
+    public ResponseEntity<CustomerDto> getCustomerById(@PathVariable("id")UUID id){
+        return new ResponseEntity<CustomerDto>(customerService.getCustomerById(id), HttpStatus.OK);
+    }
+}
